1104ea2a272172d820908965368d0d3ea95a07a6
Marqui-13 <98498681+Marqui-13@users.noreply.github.com>
2022-07-28 20:42:57 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { MoralisProvider } from "react-moralis";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MoralisProvider serverUrl={process.env.REACT_APP_SERVER_URL} appId={process.env.REACT_APP_ID}>
+      <App />
+    </MoralisProvider>
   </React.StrictMode>
 )
